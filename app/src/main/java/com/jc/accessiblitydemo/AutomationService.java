024aa348f9706ecfbe5c674d3933f787ff4610b6
chenzujie <chenzujie@91yong.com>
2017-02-15 17:43:57 +0800

    注册功能

---
+++ app/src/main/java/com/jc/accessiblitydemo/AutomationService.java
@@ -0,0 +1,21 @@
+package com.jc.accessiblitydemo;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+
+public class AutomationService extends AccessibilityService{
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
