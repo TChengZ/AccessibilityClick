565f77fa0b2665819ca2bb0be30c8629dd355e25
chenzujie <chenzujie@91yong.com>
2017-02-15 16:17:39 +0800

    工程初始化

---
+++ app/src/main/java/com/jc/accessiblitydemo/AccessibilityActivity.java
@@ -0,0 +1,18 @@
+package com.jc.accessiblitydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AccessibilityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accessibility);
+    }
+
+    public void autoClick(View view){
+
+    }
+}
