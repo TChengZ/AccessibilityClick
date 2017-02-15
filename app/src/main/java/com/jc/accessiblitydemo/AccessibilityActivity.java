b16f00d59c9c68539284645e15c38da6c6acca05
chenzujie <chenzujie@91yong.com>
2017-02-15 18:56:19 +0800

    添加自动打开的功能

--- app/src/main/java/com/jc/accessiblitydemo/AccessibilityActivity.java
+++ app/src/main/java/com/jc/accessiblitydemo/AccessibilityActivity.java
@@ -1,10 +1,26 @@
 package com.jc.accessiblitydemo;
 
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class AccessibilityActivity extends Activity {
+
+    private static final String TAG = "AccessibilityActivity";
+    private Dialog mOpenDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +29,45 @@ public class AccessibilityActivity extends Activity {
     }
 
     public void autoClick(View view){
+        if(!isServiceRunning()){
+            if(null == mOpenDialog){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("打开" + getString(R.string.automation) );
+                builder.setMessage("选择[AccessibilityDemo]并打开");
+                builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        openAccessibilityServiceSettings();
+                    }
+                });
+                mOpenDialog = builder.show();
+            }
+            if(!mOpenDialog.isShowing()) {
+                mOpenDialog.show();
+            }
+        }
+    }
 
+    private void openAccessibilityServiceSettings() {
+        try {
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isServiceRunning(){
+        boolean running = false;
+        String currentServiceName = getPackageName() + "/.AutomationService";
+        AccessibilityManager accessibilityManager = (AccessibilityManager) this.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> list = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for(AccessibilityServiceInfo accessibilityServiceInfo: list){
+            if(accessibilityServiceInfo.getId().equals(currentServiceName)){
+                running = true;
+                break;
+            }
+        }
+        return running;
     }
 }
